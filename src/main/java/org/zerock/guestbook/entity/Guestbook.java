d25fba896e567d10e62d1b5e0f6c83fb7a64d8fb
hjt7446 <hjt1120@naver.com>
2022-05-31 01:32:49 +0900

    init

---
+++ src/main/java/org/zerock/guestbook/entity/Guestbook.java
@@ -0,0 +1,30 @@
+package org.zerock.guestbook.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Guestbook extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    private Long gno;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 1500, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
